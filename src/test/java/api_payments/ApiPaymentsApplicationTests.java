867a7a2957bef09e7b380081b895f789bde91244
Maycon de Aquino Oliveira Sousa <mayconaquino@ciandt.com>
2025-01-13 14:27:36 -0300

    Criando api e implrmrntsndo estrutura de pastas

---
+++ src/test/java/api_payments/ApiPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package api_payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
