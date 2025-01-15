11cc9e6c5358d27b21971477f7be7164f20db37b
Maycon de Aquino Oliveira Sousa <mayconaquino@ciandt.com>
2025-01-15 12:35:18 -0300

    Metodo gera o QR code

---
+++ src/main/java/service/QRCodeGenerationService.java
@@ -0,0 +1,32 @@
+package service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Service
+public class QRCodeGenerationService {
+
+    public  byte[] generateQRCode(String walletAddress, double amount) throws WriterException, IOException {
+        String bitcoinURI = String.format("bitcoin:%s?amount=%.8f", walletAddress, amount);
+
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        int width = 300;
+        int height = 300;
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(bitcoinURI, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return outputStream.toByteArray();
+
+    }
+}
